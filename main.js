1f1d23cadde04f478dbb50344282d8c00cbb8d0d
James Foss <jamesfoss1@hotmail.com>
2018-08-17 16:41:05 -0400

    Added Gallery & Modals

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let htmlStr1 = '';
 let htmlStr2 = '';
+//let modals;
+//let btnContainer;
+let closeBtn;
+let nextBtn;
+let prevBtn;
+let modalArray = [];
+let indexNum;
+const galleryDiv = document.getElementById('gallery');
+let cards;
+const modalDiv = document.createElement('div');
+modalDiv.className = 'modal-container';
+const $body = $('body');
+$body.append(modalDiv);
 
 $.ajax({
 url: 'https://randomuser.me/api/?results=12&nat=us',
@@ -17,7 +30,19 @@ success: function(data) {
           <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
         </div>
       </div>`;
-    htmlStr2 += `
+    modalArray.push({
+      first: data.results[i].name.first,
+      last: data.results[i].name.last,
+      image: data.results[i].picture.large,
+      email: data.results[i].email,
+      cell: data.results[i].cell,
+      street: data.results[i].location.street,
+      city: data.results[i].location.city,
+      state: data.results[i].location.state,
+      zip: data.results[i].location.postcode,
+      dob: data.results[i].dob.date
+    });
+    /*htmlStr2 += `
       <div class="modal">
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
@@ -30,20 +55,23 @@ success: function(data) {
               <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} ${data.results[i].location.postcode}</p>
               <p class="modal-text">${data.results[i].dob.date}</p>
           </div>
-      </div>`;
+      </div>`;*/
   }
-  htmlStr2 += `
+  /*htmlStr2 += `
     <div class="modal-btn-container">
         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>`;
-  const modalDiv = document.createElement('div');
-  modalDiv.className = 'modal-container';
-  modalDiv.innerHTML = htmlStr2;
-  const galleryDiv = document.getElementById('gallery');
+    </div>`;*/
+  //modalDiv.innerHTML = htmlStr2;
+  //btnContainer = modalDiv.querySelector('.modal-btn-container');
+  //modals = modalDiv.children;
+  //closeBtn = modalDiv.querySelectorAll('')
+  /*for (i = 0; i < modals.length; i += 1) {
+    modals[i].style.display = 'none';
+  }*/
+  modalDiv.style.display = 'none';
   galleryDiv.innerHTML = htmlStr1;
-  const $body = $('body');
-  $body.append(modalDiv);
+  cards = galleryDiv.children;
 }
 });
 
@@ -56,8 +84,83 @@ const $searchContainer = $('.search-container');
 $searchContainer.html(searchBox);
 
 
+galleryDiv.addEventListener('click', (e) => {
+  for (i = 0; i < cards.length; i += 1) {
+    if (e.target === cards[i]) {
+      createModal(i);
+    }
+  }
+});
+
+/*function showModals (index) {
+  for (let i = 0; i < modals.length; i += 1) {
+    if (i === index) {
+      modals[i].style.display = 'block';
+      modalDiv.style.display = 'block';
+      btnContainer.style.display = 'block';
+    }
+  }
+}*/
+
+function createModal (index) {
+  indexNum = index;
+  htmlStr2 = `
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn closeBtn"><strong class="closeBtn">X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${modalArray[index].image}" alt="${modalArray[index].first} ${modalArray[index].last} profile picture">
+            <h3 id="name" class="modal-name cap">${modalArray[index].first} ${modalArray[index].last}</h3>
+            <p class="modal-text">${modalArray[index].email}</p>
+            <p class="modal-text cap">${modalArray[index].city}</p>
+            <hr>
+            <p class="modal-text">${modalArray[index].cell}</p>
+            <p class="modal-text">${modalArray[index].street}, ${modalArray[index].city}, ${modalArray[index].state} ${modalArray[index].zip}</p>
+            <p class="modal-text">${modalArray[index].dob}</p>
+        </div>
+    </div>
+    <div class="modal-btn-container">`
+  if (index === cards.length - 1) {
+    htmlStr2 += `
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" disabled=true id="modal-next" class="modal-next disbtn">Next</button>
+      </div>`;
+  } else if (index === 0) {
+    htmlStr2 += `
+        <button type="button" disabled=true id="modal-prev" class="modal-prev disbtn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>`;
+  } else {
+    htmlStr2 += `
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>`;
+  }
+  modalDiv.innerHTML = htmlStr2;
+  modalDiv.style.display = 'block';
+  //closeBtn = document.getElementById('modal-close-btn');
+  //nextBtn = document.getElementById('modal-next');
+  //prevBtn = document.getElementById('modal-prev');
 
 
+
+  /*closeBtn.addEventListener('click', (e) => {
+    modalDiv.innerHTML = '';
+    modalDiv.style.display = 'none';
+  });
+  nextBtn.addEventListener('click', (e) => {
+    createModal(index + 1);
+  });*/
+}
+modalDiv.addEventListener('click', (e) => {
+  if (e.target.id === 'modal-next') {
+    createModal(indexNum + 1);
+  } else if (e.target.id === 'modal-prev') {
+    createModal(indexNum - 1);
+  } else if (e.target.classList.contains('closeBtn')) {
+    modalDiv.innerHTML = '';
+    modalDiv.style.display = 'none';
+  }
+});
 
 // Create main components to the app and add them dynamically.
 // Use the markup in the index.html file to create components.
