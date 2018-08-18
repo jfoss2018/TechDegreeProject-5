c363d430a9b6ce34e4d6107fd04749c6e3209415
James Foss <jamesfoss1@hotmail.com>
2018-08-18 01:06:18 -0400

    Project Complete

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # TechDegreeProject-5
+
+
+This project uses an API to collect random user information from randomuser.me.
+
+The purpose is to successfully collect that information, and display it on the webpage as if it were a distributed company's users' contact information. 
+There are other challenges which include creating more detailed modal windows and a search box dynamically.
+
+The javascript functionality and styling changes I've made are listed below:
+
+--The get request returns a json object with 12 results. We were instructed to collect 12 random users' information.
+--I iterate over the 12 users to build a string containing HTML to add to the contact card gallery. At the same time, I use a three random numbers to create a radial
+	gradient background image color for each card. I collect the json data and color data as objects within arrays to use later to create the modals.
+--Upon hover over any "card" div, I used CSS transitions to transform the "card" to a 1.1 scale.
+--When a card is clicked, I use a function to pass the index number of the child node clicked to a function to build the modal. That index number corresponds to the
+	index number collected when creating the cards. When building an html string, I use a conditional statement to determine the index number position within the
+	search array to disable the previous, next, or both buttons.
+--The close button resets the modal div's html to nothing, and the previous/next buttons rerun the create modal function with the next or previous index number within
+	the search array.
+--The search box will limit the search array by iterating over the "cards" and checking to see if the search box value is included within the name date associated with
+	each "card." If the search criteria is included, the corresponding index number gets pushed onto the search array. If there are no search results, a message
+	is displayed notifying the user.
+--The colors produced for the cards and the modals are limited to each rgb value being between 155 and 255 in order to maintain a light color.
--- main.js
+++ main.js
@@ -1,83 +1,121 @@
+!function () {
+
+// Initial variable declarations.
 let htmlStr1 = '';
 let htmlStr2 = '';
-let closeBtn;
-let nextBtn;
-let prevBtn;
 let modalArray = [];
+let colorArray = [];
 let indexNum;
 let searchArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const galleryDiv = document.getElementById('gallery');
 let cards;
+
+// Inital Element selection.
+const galleryDiv = document.getElementById('gallery');
+
+// Create and append the modal container div.
 const modalDiv = document.createElement('div');
 modalDiv.className = 'modal-container';
 const $body = $('body');
 $body.append(modalDiv);
 
-$.ajax({
-url: 'https://randomuser.me/api/?results=12&nat=us',
-dataType: 'json',
-success: function(data) {
-  for (let i = 0; i < 12; i += 1) {
-    htmlStr1 += `
-      <div class="card">
-        <div class="card-img-container">
-          <img class="card-img" src="${data.results[i].picture.large}" alt="${data.results[i].name.first} ${data.results[i].name.last} profile picture">
-        </div>
-        <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-          <p class="card-text">${data.results[i].email}</p>
-          <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
-        </div>
-      </div>`;
-    modalArray.push({
-      first: data.results[i].name.first,
-      last: data.results[i].name.last,
-      image: data.results[i].picture.large,
-      email: data.results[i].email,
-      cell: data.results[i].cell,
-      street: data.results[i].location.street,
-      city: data.results[i].location.city,
-      state: data.results[i].location.state,
-      zip: data.results[i].location.postcode,
-      dob: data.results[i].dob.date
-    });
-  }
-  modalDiv.style.display = 'none';
-  galleryDiv.innerHTML = htmlStr1;
-  cards = galleryDiv.children;
-}
-});
+// Create and append the "no search results" message div.
+const messageDiv = document.createElement('div');
+const parentNode = galleryDiv.parentNode;
+parentNode.insertBefore(messageDiv, galleryDiv);
 
-const searchBox = `
-  <form action="#" method="get">
+// Create and append the search box.
+const searchBox =
+  `<form action="#" method="get">
       <input type="search" id="search-input" class="search-input" placeholder="Search...">
       <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
   </form>`;
 const $searchContainer = $('.search-container');
 $searchContainer.html(searchBox);
 
+// Element selections after creation of the search box.
 const searchInput = document.getElementById('search-input');
 const searchSubmit = document.getElementById('search-submit');
 
-searchSubmit.addEventListener('click', (e) => {
-  searchArray = [];
-  const searchText = searchInput.value;
-  for (let i = 0; i < modalArray.length; i += 1) {
-    cards[i].style.display = 'none';
-    const firstName = modalArray[i].first;
-    const lastName = modalArray[i].last;
-    if (firstName.includes(searchText) || lastName.includes(searchText)) {
-      cards[i].style.display = 'block';
-      searchArray.push(i);
+// jQuery get api that requests 12 results that include a nationality of "US." This also loops
+// through the json object to create a string with all 12 "cards" that contain the employee info.
+// I collect all the relevent data into an array for use in creating the modals, and set each card
+// to a random background gradient color. I collect the colors in an array for use in creating the
+// modals as well.
+$.ajax({
+  url: 'https://randomuser.me/api/?results=12&nat=us',
+  dataType: 'json',
+  success: function(data) {
+    for (let i = 0; i < 12; i += 1) {
+      htmlStr1 += `
+        <div class="card">
+          <div class="card-img-container">
+            <img class="card-img" src="${data.results[i].picture.large}" alt="${data.results[i].name.first} ${data.results[i].name.last} profile picture">
+          </div>
+          <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+            <p class="card-text">${data.results[i].email}</p>
+            <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+          </div>
+        </div>`;
+      modalArray.push({
+        first: data.results[i].name.first,
+        last: data.results[i].name.last,
+        image: data.results[i].picture.large,
+        email: data.results[i].email,
+        cell: data.results[i].cell,
+        street: data.results[i].location.street,
+        city: data.results[i].location.city,
+        state: data.results[i].location.state,
+        zip: data.results[i].location.postcode,
+        dob: data.results[i].dob.date
+      });
+    }
+    modalDiv.style.display = 'none';
+    galleryDiv.innerHTML = htmlStr1;
+    cards = galleryDiv.children;
+    for (let i = 0; i < cards.length; i += 1) {
+      const rNum = Math.random()*100+155;
+      const gNum = Math.random()*100+155;
+      const bNum = Math.random()*100+155;
+      cards[i].style.backgroundImage = `radial-gradient(transparent 60%, rgba(${rNum}, ${gNum}, ${bNum}, 1))`;
+      colorArray.push({
+        red: rNum,
+        green: gNum,
+        blue: bNum
+      })
     }
   }
 });
 
+// This Event Listener collects information typed into the search bar, and checks to see
+// if any first or last name includes the typed string. It also collects which index number
+// for each card that displays in order to limit which modals will display later.
+searchSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchArray = [];
+  const searchText = searchInput.value.toUpperCase();
+  for (let i = 0; i < modalArray.length; i += 1) {
+    cards[i].style.display = 'none';
+    const fullName = `${modalArray[i].first.toUpperCase()} ${modalArray[i].last.toUpperCase()}`;
+    //const lastName = modalArray[i].last.toUpperCase();
+    //if (firstName.includes(searchText) || lastName.includes(searchText)) {
+    if (fullName.includes(searchText)) {
+      cards[i].style.display = 'flex';
+      searchArray.push(i);
+    }
+  }
+  if (searchArray.length === 0) {
+    messageDiv.style.margin = '50px 0 0 0';
+    messageDiv.innerHTML =
+      `<span class='noresult'>There are no results for your search criteria. Please try again.</span>`;
+  } else {
+    messageDiv.style.margin = '0';
+    messageDiv.innerHTML = '';
+  }
+});
 
-
-
-
-
+// This Event Listener passes the index number of the card that was clicked
+// to the createModal function.
 galleryDiv.addEventListener('click', (e) => {
   for (i = 0; i < cards.length; i += 1) {
     if (e.target === cards[i]) {
@@ -86,6 +124,26 @@ galleryDiv.addEventListener('click', (e) => {
   }
 });
 
+// This Event Listener handles all button clicks made on the modal window.
+// If the next or previous buttons are clicked, it calls the createModal function
+// If the close button is clicked, it closes.
+modalDiv.addEventListener('click', (e) => {
+  if (e.target.id === 'modal-next') {
+    createModal(searchArray[indexNum + 1]);
+  } else if (e.target.id === 'modal-prev') {
+    createModal(searchArray[indexNum - 1]);
+  } else if (e.target.classList.contains('closeBtn')) {
+    modalDiv.innerHTML = '';
+    modalDiv.style.display = 'none';
+  }
+});
+
+// This function uses the index number passed to it in addition to the modalArray created earlier
+// to create a modal window. Each time a new modal window is opened, the previous one is overwritten.
+// The html that gets added for the previous and next buttons depend on the position of index number within
+// the searchArray array. If you're at the beginning, the previous button is disabled, and the same for the
+// end with the next button. This function also sets the background gradient with the previously collected
+// color info.
 function createModal (index) {
   indexNum = searchArray.indexOf(index);
   htmlStr2 = `
@@ -126,54 +184,11 @@ function createModal (index) {
   }
   modalDiv.innerHTML = htmlStr2;
   modalDiv.style.display = 'block';
+  const modalCard = modalDiv.querySelector('.modal');
+  const btnContainer = modalDiv.querySelector('.modal-btn-container');
+  modalCard.style.backgroundImage =
+    `radial-gradient(transparent 60%, rgba(${colorArray[index].red}, ${colorArray[index].green}, ${colorArray[index].blue}, 1))`;
+  btnContainer.style.background = `rgba(${colorArray[index].red}, ${colorArray[index].green}, ${colorArray[index].blue}, 1)`;
 }
 
-modalDiv.addEventListener('click', (e) => {
-  if (e.target.id === 'modal-next') {
-    createModal(searchArray[indexNum + 1]);
-  } else if (e.target.id === 'modal-prev') {
-    createModal(searchArray[indexNum - 1]);
-  } else if (e.target.classList.contains('closeBtn')) {
-    modalDiv.innerHTML = '';
-    modalDiv.style.display = 'none';
-  }
-});
-
-
-
-
-
-
-// Create main components to the app and add them dynamically.
-// Use the markup in the index.html file to create components.
-
-// Add things like color, background color, font, shadows,
-// transitions and animations to make this project your own.
-
-// Use the random user generator API to generate 12 users *** make sure
-// to filter by nationality in order to only include English names *** and display
-// Image
-// First and Last name
-// email
-// City or Location
-
-// When any employee is clicked, a modal window should pop up with
-// additional information. Be sure to include a way to close the window.
-// All information listed above, plus
-// Cell Number
-// Detailed Address
-// Birthday
-
-// Add good code comments.
-
-// Check for cross-browser consistency.
-
-// Extra Credit: Add a search box in order to search directory by name.
-
-// Extra Credit: Add a toggle feature to the modal window in order to toggle
-// trough the list of employees. There should be no error when the beginning
-// or ending of the list is reached.
-
-// Extra Credit: Must make some changes in appearance and document those changes
-// in the readme file. Also explain what the project is, and list any additional
-// information that users may need in the readme.
+}();
